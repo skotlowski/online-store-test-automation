eab78fc14fa88cae95e219121951935667f7a34e
Szymon Kotlowski <szymonkotlowski20@gmail.com>
2022-06-07 19:17:41 +0200

    added classes to test files

--- tests/test_login.py
+++ tests/test_login.py
@@ -3,16 +3,18 @@ from pytest import mark
 
 
 @mark.request
-def test_login(url, login_and_password):
-    session = Session()
-    session.headers.update(
-        {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36',
-        })
+class LoginTests:
 
-    expected_response = 200
+    def test_login(self, url, login_and_password):
+        session = Session()
+        session.headers.update(
+            {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36',
+            })
 
-    session.get(url=url)
-    response = session.post(url=f'{url}/login_check', data=login_and_password)
+        expected_response = 200
 
-    assert response.status_code == expected_response
+        session.get(url=url)
+        response = session.post(url=f'{url}/login_check', data=login_and_password)
+
+        assert response.status_code == expected_response
--- tests/test_storage.py
+++ tests/test_storage.py
@@ -3,32 +3,33 @@ from selenium.webdriver.common.by import By
 
 
 @mark.browser
-def test_storage_add_new_item(url, browser_logged):
-    browser = browser_logged
+class StorageTests:
 
-    # Clean storage, this method is not working on website - bug
-    # clear_storage(browser, url)
+    def test_storage_add_new_item(self, url, browser_logged):
+        browser = browser_logged
 
-    # Navigate to TV section
-    browser.get(url=f'{url}/telewizory-i-rtv/telewizory')
+        # Clean storage, this method is not working on website - bug
+        # clear_storage(browser, url)
 
-    # Add first item to storage
-    browser.find_element(by=By.CSS_SELECTOR, value="[class='c-offerBox_photo'").click()
-    expected_item = browser.find_element(by=By.CSS_SELECTOR, value="[class='a-typo is-primary'").text
-    browser.find_element(by=By.CSS_SELECTOR, value="[class ='c-btn is-link is-addToWishlist'").click()
+        # Navigate to TV section
+        browser.get(url=f'{url}/telewizory-i-rtv/telewizory')
 
-    # Navigate to storage
-    browser.get(url=f'{url}/ulubione/storage')
-    item = browser.find_element(by=By.CSS_SELECTOR, value="[class='a-typo is-text'").text
-    browser.find_element(by=By.CSS_SELECTOR, value="[class='c-btn is-link is-remove'").click()
+        # Add first item to storage
+        browser.find_element(by=By.CSS_SELECTOR, value="[class='c-offerBox_photo'").click()
+        expected_item = browser.find_element(by=By.CSS_SELECTOR, value="[class='a-typo is-primary'").text
+        browser.find_element(by=By.CSS_SELECTOR, value="[class ='c-btn is-link is-addToWishlist'").click()
 
-    assert expected_item == item
+        # Navigate to storage
+        browser.get(url=f'{url}/ulubione/storage')
+        item = browser.find_element(by=By.CSS_SELECTOR, value="[class='a-typo is-text'").text
+        browser.find_element(by=By.CSS_SELECTOR, value="[class='c-btn is-link is-remove'").click()
 
+        assert expected_item == item
 
-def clear_storage(browser_logged, url):
-    browser = browser_logged
-    # Navigate to storage
-    browser.get(url=f'{url}/ulubione/storage')
-    # Remove all items from storage
-    browser.find_element(by=By.CSS_SELECTOR, value="[class='c-btn is-link is-wishlistRemoveAll'").click()
+    def clear_storage(self, browser_logged, url):
+        browser = browser_logged
+        # Navigate to storage
+        browser.get(url=f'{url}/ulubione/storage')
+        # Remove all items from storage
+        browser.find_element(by=By.CSS_SELECTOR, value="[class='c-btn is-link is-wishlistRemoveAll'").click()
 
